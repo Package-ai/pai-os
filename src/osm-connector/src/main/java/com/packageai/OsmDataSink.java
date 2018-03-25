1a2a1e8e83c46c75cd7712a99b894e5abfa380e0
ayal.weiss <ayal@package.ai>
2018-03-25 16:41:26 +0300

    restrictions

--- src/osm-connector/src/main/java/com/packageai/OsmDataSink.java
+++ src/osm-connector/src/main/java/com/packageai/OsmDataSink.java
@@ -24,6 +24,7 @@ public class OsmDataSink implements Sink {
 
 	private List<HighwayData> ways;
 	private List<NodeData> nodes;
+	private List<RestrictionData> restrictions;
 
 	public static final Set<String> WAY_TYPES = Sets.newHashSet("motorway",
 			"trunk",
@@ -44,6 +45,7 @@ public class OsmDataSink implements Sink {
 	OsmDataSink(){
 		this.ways = new ArrayList<>();
 		this.nodes = new ArrayList<>();
+		this.restrictions = new ArrayList<>();
 	}
 
 	@Override
@@ -53,30 +55,30 @@ public class OsmDataSink implements Sink {
 		switch (type) {
 			case Way:
 				Way way = (Way) entity;
-				Map<String, String> tagValueMap = new TagCollectionImpl(way.getTags()).buildMap();
-				boolean isHighway = tagValueMap.containsKey("highway");
+				Map<String, String> wayTagValueMap = new TagCollectionImpl(way.getTags()).buildMap();
+				boolean isHighway = wayTagValueMap.containsKey("highway");
 				if (!isHighway) {
 					break;
 				}
-				String highwayType = tagValueMap.get("highway");
+				String highwayType = wayTagValueMap.get("highway");
 				if (!WAY_TYPES.contains(highwayType)) {
 					break;
 				}
 				double maxspeed = -1;
-				if (tagValueMap.containsKey("maxspeed")) {
-					String maxSpeedString = tagValueMap.get("maxspeed").split(" ")[0];
+				if (wayTagValueMap.containsKey("maxspeed")) {
+					String maxSpeedString = wayTagValueMap.get("maxspeed").split(" ")[0];
 					try {
 						maxspeed = Integer.parseInt(maxSpeedString);
 					} catch (Exception e) {
 					}
 				}
-				boolean isOneWay = tagValueMap.containsKey("oneway") && (tagValueMap.get("oneway").equalsIgnoreCase("true") || tagValueMap.get("oneway").equalsIgnoreCase("yes"));
+				boolean isOneWay = wayTagValueMap.containsKey("oneway") && (wayTagValueMap.get("oneway").equalsIgnoreCase("true") || wayTagValueMap.get("oneway").equalsIgnoreCase("yes"));
 				List<WayNode> wayNodes = way.getWayNodes();
 				List<Long> nodeIds = wayNodes.stream().map(node -> node.getNodeId()).collect(Collectors.toList());
-				boolean hasName = tagValueMap.containsKey("name");
+				boolean hasName = wayTagValueMap.containsKey("name");
 				String name = null;
 				if (hasName){
-					name = tagValueMap.get("name");
+					name = wayTagValueMap.get("name");
 				}
 
 				HighwayData highwayData = new HighwayData(maxspeed, isOneWay, highwayType, nodeIds, name);
@@ -87,6 +89,44 @@ public class OsmDataSink implements Sink {
 				long id = node.getId();
 				NodeData nodeData = new NodeData(id, node.getLatitude(), node.getLongitude());
 				nodes.add(nodeData);
+				break;
+			case Relation:
+				Relation relation = (Relation) entity;
+				Map<String, String> relationTagValueMap = new TagCollectionImpl(relation.getTags()).buildMap();
+				if (!relationTagValueMap.containsKey("restriction")){
+					break;
+				}
+				String restrictionType = relationTagValueMap.get("restriction");
+				boolean negativeRestriction = false;
+				if (restrictionType.startsWith("no")){
+					negativeRestriction = true;
+				}
+				List<RelationMember> members = relation.getMembers();
+				long from = -1;
+				long to = -1;
+				long via = -1;
+				boolean viaIsWay = false;
+				int checksum = 0;
+				for (RelationMember member : members){
+					String memberRole = member.getMemberRole();
+					if (memberRole.equals("from")){
+						from = member.getMemberId();
+						checksum++;
+					}
+					else if (memberRole.equals("to")){
+						to = member.getMemberId();
+						checksum++;
+					}
+					else if (memberRole.equals("via")){
+						viaIsWay = member.getMemberType().equals(EntityType.Way);
+						via = member.getMemberId();
+						checksum++;
+					}
+				}
+
+				if (checksum == 3){
+					restrictions.add(new RestrictionData(negativeRestriction, from, to, via, viaIsWay));
+				}
 		}
 	}
 
@@ -96,6 +136,10 @@ public class OsmDataSink implements Sink {
 
 	public List<NodeData> getNodes() {
 		return nodes;
+	}
+
+	public List<RestrictionData> getRestrictions() {
+		return restrictions;
 	}
 
 	@Override
@@ -139,7 +183,7 @@ public class OsmDataSink implements Sink {
 
 		reader.setSink(sink);
 		reader.run();
-		return new OsmParsedData(sink.getWays(), sink.getNodes());
+		return new OsmParsedData(sink.getWays(), sink.getNodes(), sink.getRestrictions());
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
